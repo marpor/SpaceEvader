cf96a83e27337c5bbccd52334ea4ea2b0f1aa153
Mark Porsby <mark@porsby.dk>
2021-11-10 16:31:35 +0100

    Simplify screenshot script

--- demos/Screenshot.gd
+++ demos/Screenshot.gd
@@ -13,71 +13,29 @@ func _ready():
 	$Heic2007a.visible = false
 
 	# Square
-	set_size(120,120)
-	yield(VisualServer, "frame_post_draw")
-	screenshot("res://export/Icon120x120.png",120,120)
-
-	set_size(76,76)
-	yield(VisualServer, "frame_post_draw")
-	screenshot("res://export/Icon76x76.png",76,76)
-
-	set_size(1024,1024)
-	yield(VisualServer, "frame_post_draw")
-	screenshot("res://export/Icon1024x1024.png",1024,1024)
+	yield(screenshot2("res://export/Icon120x120.png",120,120), "completed")
+	yield(screenshot2("res://export/Icon76x76.png",76,76), "completed")
+	yield(screenshot2("res://export/Icon1024x1024.png",1024,1024), "completed")
 
 	$Heic2007a.visible = true
 
 	# Landscape
-	set_size(2436,1125)
-	yield(VisualServer, "frame_post_draw")
-	screenshot("res://export/iphone_2436x1125.png",2436,1125)
-
-	set_size(2208,1242)
-	yield(VisualServer, "frame_post_draw")
-	screenshot("res://export/iphone_2208x1242.png",2208,1242)
-
-	set_size(2436,1125)
-	yield(VisualServer, "frame_post_draw")
-	screenshot("res://export/iphone_2436x1125.png",2436,1125)
-
-	set_size(1024, 768)
-	yield(VisualServer, "frame_post_draw")
-	screenshot("res://export/ipad_1024x768.png",1024, 768)
-
-	set_size(2048,1536)
-	yield(VisualServer, "frame_post_draw")
-	screenshot("res://export/ipad_2048x1536.png",2048,1536)
+	yield(screenshot2("res://export/iphone_2436x1125.png",2436,1125), "completed")
+	yield(screenshot2("res://export/iphone_2208x1242.png",2208,1242), "completed")
+	yield(screenshot2("res://export/iphone_2436x1125.png",2436,1125), "completed")
+	yield(screenshot2("res://export/ipad_1024x768.png",1024, 768), "completed")
+	yield(screenshot2("res://export/ipad_2048x1536.png",2048,1536), "completed")
 
 	# Portrait
-	set_size(640,960)
-	yield(VisualServer, "frame_post_draw")
-	screenshot("res://export/iphone_640x960.png",640,960)
+	yield(screenshot2("res://export/iphone_640x960.png",640,960), "completed")
+	yield(screenshot2("res://export/iphone_640x1136.png",640,1136), "completed")
+	yield(screenshot2("res://export/iphone_750x1334.png",750,1334), "completed")
+	yield(screenshot2("res://export/iphone_1125x2436.png",1125,2436), "completed")
+	yield(screenshot2("res://export/ipad_768x1024.png",768,1024), "completed")
+	yield(screenshot2("res://export/ipad_1536x2048.png",1536,2048), "completed")
+	yield(screenshot2("res://export/iphone_1242x2208.png",1242,2208), "completed")
 
-	set_size(640,1136)
-	yield(VisualServer, "frame_post_draw")
-	screenshot("res://export/iphone_640x1136.png",640,1136)
-
-	set_size(750,1334)
-	yield(VisualServer, "frame_post_draw")
-	screenshot("res://export/iphone_750x1334.png",750,1334)
-
-	set_size(1125,2436)
-	yield(VisualServer, "frame_post_draw")
-	screenshot("res://export/iphone_1125x2436.png",1125,2436)
-
-	set_size(768,1024)
-	yield(VisualServer, "frame_post_draw")
-	screenshot("res://export/ipad_768x1024.png",768,1024)
-
-	set_size(1536,2048)
-	yield(VisualServer, "frame_post_draw")
-	screenshot("res://export/ipad_1536x2048.png",1536,2048)
-
-	set_size(1242,2208)
-	yield(VisualServer, "frame_post_draw")
-	screenshot("res://export/iphone_1242x2208.png",1242,2208)
-	
-#	get_tree().quit()
+	get_tree().quit()
 
 func _on_viewport_size_changed():
 	# Do whatever you need to do when the window changes!
@@ -101,6 +59,13 @@ func screenshot(filename, w=0, h=0):
 
 	# Flip it on the y-axis (because it's flipped).
 	img.flip_y()
+	
+	# Crop - needed for odd sizes (e.g. for w=1125 get_data returns 1126 wide 
 	img.crop(w,h)
 
 	img.save_png(filename)
+
+func screenshot2(filename, w,h):
+	set_size(w,h)
+	yield(VisualServer, "frame_post_draw")
+	screenshot(filename, w,h)
