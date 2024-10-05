7ba7f4e3d2f9749f23c01c6f63071928f8ab8437
marpor <marpor@users.noreply.github.com>
2024-10-05 10:45:57 +0000

    Deploying to gh-pages from @ marpor/SpaceEvader@5ba20311b34bd08de3ce1f20325918e7f56ded64 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1727519651|31486679";
+const CACHE_VERSION = "1728125145|31635313";
 const CACHE_PREFIX = "SpaceEvader-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
